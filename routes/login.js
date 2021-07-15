90ce88c87da6f2dcfe672ad9d628393669df3287
uju_sy <lsyeon1997@ajou.ac.kr>
2021-07-15 18:10:36 +0900

    fix login to exception

--- routes/login.js
+++ routes/login.js
@@ -2,7 +2,7 @@ const express = require('express');
 const db = require('../db/index');
 const router = express.Router();
 
-router.post('/', async (req, res) => {
+router.post('/', async (req, res, next) => {
     const reqId = req.body.id;
     const reqPw = req.body.pw;
 
@@ -11,6 +11,9 @@ router.post('/', async (req, res) => {
     let result;
     try {
         result = await db.query(sql, reqId);
+
+        next();
+        if(result.length === 0) return res.status(400).send('LoginFailed');
 
         const resPw = result[0].user_pw;
 
